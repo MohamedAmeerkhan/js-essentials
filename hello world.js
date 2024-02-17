3694aa747c18cc2d029344012578002f6d017238
MohamedAmeerkhan <mohamedameerkhank@gmail.com>
2024-02-17 18:10:46 +0530

    sample

---
+++ hello world.js
@@ -0,0 +1,15 @@
+console.log("JavaScript");
+let greeting="Welcome to JS";
+console.log(greeting);
+
+var name="Devops", names="Developer";
+console.log(name, names);
+console.log();
+var _name='Cloud';
+var $name='AWS';
+var NAME='Docker';
+var undefined;
+console.log(undefined);
+console.log(NAME);
+
+
